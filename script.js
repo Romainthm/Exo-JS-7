0e096c1f77d01f3086928ebf7cd86384bc2b64f2
romain <romain.thomine007@gmail.com>
2019-04-12 09:10:53 +0200

    corrigé-commenté

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-function calcule() {
-  alert("Résultat : " + ( ( firstNumber.value * 2 + 5) * 50 - secondNumber.value + 1766 ));
+function calcul() {
+  //dans la variable shoeSize je récupére la valeur du premier champ idem pour yearOfBirth.
+  var shoeSize = document.getElementById('shoeSize').value;
+  var yearOfBirth = document.getElementById('yearOfBirth').value;
+  //si les champ sont remplie et si l'utilisateur a bien remplie avec des chiffre
+  if (shoeSize && yearOfBirth && !isNaN(shoeSize) && !isNaN(yearOfBirth) ){
+    //je retourne le resultat de mon calcul ci desous
+    return (shoeSize * 2 + 5) * 50 - yearOfBirth + 1766;
+  }
 }
+//je vérifie si l'utilisateur appuie sur le bouton validé si c'est le cas j'apelle ma fonction calcul
+document.getElementById('submit').onclick = function(){alert(calcul())}
